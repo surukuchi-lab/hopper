cdf40f4ff1a0a8fde57941f5d359124f507ce374
Jan Küpperbusch <jankup@gmx.de>
2026-07-27 17:45:28 +0200

    Demonstrator script for pileup, specifications of the branch added in README

--- README.md
+++ README.md
@@ -21,3 +21,24 @@ pip install -e .
 
 ## Run
 hopper-sim configs/example.yaml
+
+## Branch-specific features
+
+The pileup-integration branch features
+- Slight increase in verbosity (Mainly due to debug purposes)
+
+- Additional Configurations
+
+- Configuration refactor "electron" -> "electrons" 
+  - Dictionary with one or multiple keys, mapping int->ElectronConfig
+  - Downwards compatible with the Electron configuration. A property in config.py is added, which automatically maps this configuration tree electron -> electrons = {0: electron}
+  - Track length parameter is split up in track_length_s (simulation duration, should rename that on closer look ^^) and track_length_e (in the ElectronConfig). Sanity check built in that an electrontrack cannot go past the simulation duration 
+  - signal.cyclotron_phase0_rad and electron.cyclotron_phase0_rad are merged as they should be the same thing
+
+- Scriptable function "run_pipeline_scriptable" in pipeline.py
+  - Takes the configuration of the detector from YAML, the configuration of particles from the second argument, which is the mentioned "electrons" dictionary
+
+- Pileup capability according to mentioned electrons configuration tree
+  - Processing of the electrons is done in a for loop on the node level, both in the "dynamics" and "signal" node
+  - Output of the nodes does not change for the original run_pipeline. Next to ctx["signal_result"], there is a possibility to switch on a feature extracting the waveforms generated from each particle via ctx["individual_signals"], this has to be enabled in the configuration though (signal["save_ind_signals"])
+
--- scripts/pileup_test.py
+++ scripts/pileup_test.py
@@ -7,39 +7,43 @@ import hopper.nodes.pipeline as pipe
 import numpy as np
 plt.style.use("ibm")
 
+
+f_c_prior = 560334450
+
 electrons = {
     0: {
         "energy_eV": 18563.251,
-        "pitch_angle_deg": 89.9,
+        "pitch_angle_deg": 87,
         "r0_m": 0.16,
         "phi0_rad": 0.0,
         "z0_m": 0.0,
         "vpar_sign": 1,
         "starting_time_e": 0.0,
-        "track_length_e": 1e-6,
+        "track_length_e": 1e-5,
         "cyclotron_phase0_rad": 0,
     },
     1: {"energy_eV": 18563.251,
-        "pitch_angle_deg": 89.9,
+        "pitch_angle_deg": 87,
         "r0_m": 0.16,
         "phi0_rad": 0.0,
         "z0_m": 0.0,
         "vpar_sign": 1,
-        "starting_time_e": 0.25/(560e6),
-        "track_length_e": 1e-6,
-        "cyclotron_phase0_rad": 0,
+        "starting_time_e": 560/f_c_prior, #1e-6
+        "track_length_e": 1e-5,
+        "cyclotron_phase0_rad": np.pi/2,
     },
     2: {"energy_eV": 18563.251,
-        "pitch_angle_deg": 89.9,
+        "pitch_angle_deg": 87,
         "r0_m": 0.16,
         "phi0_rad": 0.0,
         "z0_m": 0.0,
         "vpar_sign": 1,
-        "starting_time_e": 4.1/(560e6),
-        "track_length_e": 1e-6,
-        "cyclotron_phase0_rad": 0,
+        "starting_time_e": 1120/f_c_prior, #2e-6
+        "track_length_e": 1e-5,
+        "cyclotron_phase0_rad": np.pi,
     },
 }
+
 
 plot_ds = 1
 start = time.perf_counter()
@@ -48,14 +52,14 @@ end = time.perf_counter()
 print(f"run_pipeline_scriptable timing benchmark is {end - start:.6f} s")
 
 output = {"meta": meta, "signal": signal, "signal_dict": signal_dict}
-np.savez('integration', output)
+np.savez('integration_no_ringup', output)
 
 #plt.plot(signal.t_if, signal.iq_if, label=f"iq_if")
 #plt.scatter(signal.t, signal.iq, marker=".", label=f"iq {i}")
 
 
-#window = (0, track.t[-1])#(100/(560e6), 500/(560e6)) 
-window = (0, 20/(560e6))
+window = (-1e-8, 3e-6)#signal.t[-1])#(100/(560e6), 500/(560e6)) 
+#window = (0, 20/(560e6))
 
 nplots = 4
 plt.figure(figsize=(10, 8))
@@ -65,11 +69,24 @@ plt.plot(signal.t[::plot_ds], signal.iq[::plot_ds], linewidth=2, alpha=0.8, labe
 plt.xlim(window)
 plt.legend(loc=1)
 for i in signal_dict:
-    plt.subplot(nplots, 1, i+2)
+    el = signal_dict[i]
+    track_info = el.track_if
+    f_c = np.mean(track_info.f_c_hz[:100])
+    print(f_c)
+    plt.subplot(nplots, 2, 2*i+3)
     #plt.axvline(electrons[i]["starting_time_e"])
+    starting_t = electrons[i]["starting_time_e"]
+    phi_glob = electrons[i]["cyclotron_phase0_rad"]
+    plt.title(f"omega_c = {f_c:.2e} Hz, phi = {(phi_glob%(2*np.pi))/(2*np.pi)*360:.2f}°")
     plt.plot(signal_dict[i].t[::plot_ds], signal_dict[i].iq[::plot_ds], linewidth=2, alpha=0.8, label=f"iq {i} [V]")
     plt.xlim(window)
     plt.legend(loc=1)
-plt.tight_layout()
-plt.savefig("../imgs/3_el.png")
+    plt.subplot(nplots, 2, 2*i+4)
+    #plt.axvline(electrons[i]["starting_time_e"])
+    plt.title(f"Starting time {starting_t:.2e} zoomed in")
+    plt.plot(signal_dict[0].t[::plot_ds], signal_dict[0].iq[::plot_ds], linewidth=2, alpha=0.8, label=f"iq 0 [V]")
+    plt.plot(signal_dict[i].t[::plot_ds], signal_dict[i].iq[::plot_ds], linewidth=2, alpha=0.8, label=f"iq {i} [V]")
+    plt.xlim(starting_t-3e-9, starting_t+1e-8)
+    plt.tight_layout()
+plt.savefig("../imgs/3_el_no_ringup_87.png")
 plt.show()
